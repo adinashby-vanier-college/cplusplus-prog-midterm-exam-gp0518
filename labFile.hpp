52400313e870110e3736341c75d14c318d2c3f2c
gp0518 <158082239+gp0518@users.noreply.github.com>
2024-03-04 12:46:47 -0500

    Update labFile.hpp

--- labFile.hpp
+++ labFile.hpp
@@ -8,23 +8,68 @@
 using namespace std;
 
 double fahrenheitToKelvin(double fahrenheit) {
-    return 0;
+    double kel;
+    kel = (fahrenheit - 32) * 5 / 9 + 273.15;
+    return kel;
 }
 
 bool isPrime(int n) {
-    return false;
+    int i;
+    bool ans = true;
+
+    if (n == 0 || n == 1) {
+        ans = false;
+    }
+    else if( n >= 1 ){
+        for (i = 2; i <= n / 2; i++) {
+            if (n % i == 0) {
+                ans = false;
+            }
+
+        }
+    }
+
+    else ans = true;
+    
+    
+    return ans;
 }
 
 int sumOfPrimes(int limit) {
-    return 0;
+    int sum = 0, n;
+    for (n = 2; n <= limit; n++) {
+        if (isPrime(n) == true) {
+            sum = n + sum;
+        }
+    }
+
+ 
+ 
+        
+    
+
+    return sum;
 }
 
+
 int reverseInteger(int num) {
+
+
     return 0;
 }
 
 void printInvertedHalfStarPyramid(int rows) {
 
+
+    switch (rows) {
+    case (1): cout << "*" << endl;
+    case (2): cout << "* *\n\n*" << endl;
+    case (3): cout << "* * *\n\n* *\n\n*" << endl;
+    case (4): cout << "* * * *\n\n* * *\n\n* *\n\n*" << endl;
+    case (5): cout << "* * * * *\n\n* * * *\n\n* * *\n\n* *\n*" << endl;
+        
+    }
+    return;
 }
 
 
